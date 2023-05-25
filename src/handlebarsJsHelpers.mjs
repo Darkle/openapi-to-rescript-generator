df35324f3cad119436856aa4e347f176d2931ffd
Coop <coop.coding@gmail.com>
2023-05-25 12:17:02 +1000

    Made the handlebars helpers more specific

--- src/handlebarsJsHelpers.mjs
+++ src/handlebarsJsHelpers.mjs
@@ -1,33 +1,40 @@
 import handlebars from 'handlebars'
 
 // https://rescript-lang.org/docs/manual/latest/reserved-keywords
+// https://github.com/rescript-lang/rescript-compiler/blob/master/jscomp/syntax/src/res_token.ml
 const isReservedKeyword = word =>
-  /^(and|as|assert|constraint|else|exception|external|false|for|if|in|include|lazy|let|module|mutable|of|open|rec|switch|true|try|type|when|while|with)$/gu.test(
+  /^(and|as|assert|await|constraint|else|exception|external|false|for|if|in|include|lazy|let|module|mutable|of|open|rec|switch|true|try|type|when|while|with)$/gu.test(
     word
   )
 
 // https://stackoverflow.com/a/69874941/2785644
 const isCapetilized = str => /\p{Lu}/u.test(str)
 
-const hasNonAsciiChars = str => /[^a-zA-Z]/gu.test(str)
+const hasNonAtoZChars = str => /[^a-zA-Z]/gu.test(str)
+
+const removeNonAtoZChars = str => str.replaceAll(/[^a-zA-Z]/gu, '')
 
 const fixIfReservedKeyword = str => (isReservedKeyword(str) ? `${str}_` : str)
 
+const upperCaseFirstLetter = str => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
+
+const lowerCaseFirstLetter = str => `${str.charAt(0).toLowerCase()}${str.slice(1)}`
+
 // eslint-disable-next-line max-lines-per-function
 function registerJSHandlebarHelpers() {
-  handlebars.registerHelper('capitalize', aString => {
+  handlebars.registerHelper('createModuleName', aString => {
     if (!aString) {
-      throw new Error("🚨 can't capitalize an undefined argument in capitalize handlebars helper")
+      throw new Error('🚨 undefined argument in createModuleName handlebars helper')
     }
     if (!aString?.length) {
-      throw new Error("🚨 can't capitalize an empty argument in capitalize handlebars helper")
+      throw new Error('🚨 empty argument in createModuleName handlebars helper')
     }
 
     // Don't need fixIfReservedKeyword as we are uppercasing first letter
-    return `${aString.charAt(0).toUpperCase()}${aString.slice(1)}`
+    return upperCaseFirstLetter(removeNonAtoZChars(aString))
   })
 
-  handlebars.registerHelper('unCapitalize', aString => {
+  handlebars.registerHelper('createReqBodyStructName', aString => {
     if (!aString) {
       throw new Error("🚨 can't unCapitalize an undefined argument in unCapitalize handlebars helper")
     }
@@ -35,26 +42,14 @@ function registerJSHandlebarHelpers() {
       throw new Error("🚨 can't unCapitalize an empty argument in unCapitalize handlebars helper")
     }
 
-    return fixIfReservedKeyword(`${aString.charAt(0).toLowerCase()}${aString.slice(1)}`)
-  })
-
-  handlebars.registerHelper('toAtoZonly', aString => {
-    if (!aString) {
-      throw new Error('🚨 no valid argument suppled to toAtoZonly handlebars helper')
-    }
-    if (!aString?.length) {
-      throw new Error('🚨 empty string supplied to toAtoZonly handlebars helper')
-    }
-
-    // Don't need fixIfReservedKeyword here as it is being used in between other characters in var creation
-    return aString.replaceAll(/[^a-zA-Z]/gu, '')
+    return fixIfReservedKeyword(lowerCaseFirstLetter(removeNonAtoZChars(aString)))
   })
 
   /*****
     Can't change the names of the api type keys as they need to match up, so instead use this trick
-    from the docs: https://rescript-lang.org/docs/manual/latest/use-illegal-identifier-names
+    from the docs: https://rescript-lang.org/syntax-lookup#as-decorator
   *****/
-  // eslint-disable-next-line complexity
+  // eslint-disable-next-line complexity, max-lines-per-function
   handlebars.registerHelper('toValidRecordFieldName', aString => {
     if (!aString) {
       throw new Error('🚨 no valid argument suppled to toValidRecordFieldName handlebars helper')
@@ -63,9 +58,29 @@ function registerJSHandlebarHelpers() {
       throw new Error('🚨 empty string supplied to toValidRecordFieldName handlebars helper')
     }
 
-    return isCapetilized(aString) || isReservedKeyword(aString) || hasNonAsciiChars(aString)
-      ? `\\"${aString}"`
-      : aString
+    const aStringOrig = aString
+
+    let isLegitName = true
+
+    if (isCapetilized(aString)) {
+      isLegitName = false
+      aString = lowerCaseFirstLetter(aString)
+    }
+
+    if (hasNonAtoZChars(aString)) {
+      isLegitName = false
+      // Last resort
+      // https://rescript-lang.org/docs/manual/latest/use-illegal-identifier-names
+      aString = `\\"${aString}"`
+    }
+
+    // Has to be after check above, otherwise the underscore will trigger the hasNonAtoZChars(aString) check
+    if (isReservedKeyword(aString)) {
+      isLegitName = false
+      aString = `${aString}_`
+    }
+
+    return isLegitName ? aStringOrig : `@as("${aStringOrig}") ${aString}`
   })
 
   handlebars.registerHelper('paramContainsParamType', (paramType, params) => {
