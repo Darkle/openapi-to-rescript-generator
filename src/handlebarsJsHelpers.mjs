32f4117eae67292d9df08cb90f727f94d0b70453
Coop <coop.coding@gmail.com>
2023-05-25 14:10:23 +1000

    Comments

--- src/handlebarsJsHelpers.mjs
+++ src/handlebarsJsHelpers.mjs
@@ -72,7 +72,6 @@ function registerJSHandlebarHelpers() {
 
     if (hasNonAtoZChars(aString)) {
       isLegitName = false
-      // Last resort
       // https://rescript-lang.org/docs/manual/latest/use-illegal-identifier-names
       aString = `\\"${aString}"`
     }
